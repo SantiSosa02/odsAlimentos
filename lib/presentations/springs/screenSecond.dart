659b0e379829048ffa9a5fa00869ad9c86ec42dd
MartPc <mpinzonc07@gmail.com>
2023-08-18 06:47:52 -0500

    commit_martin

--- lib/presentations/springs/screenSecond.dart
+++ lib/presentations/springs/screenSecond.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:ods_alimentos/presentations/windgets/menu_appbar.dart';
 import 'package:ods_alimentos/presentations/windgets/menu_drawer.dart';
+import 'package:intl/intl.dart'; 
 
 class ScreenSecond extends StatefulWidget {
   const ScreenSecond({Key? key}) : super(key: key);
 
-  @override
-  State<ScreenSecond> createState() => _ScreenSecondState();
+@override
+State<ScreenSecond> createState() => _ScreenSecondState();
 }
 
 class _ScreenSecondState extends State<ScreenSecond> {
@@ -92,34 +93,51 @@ class _ScreenSecondState extends State<ScreenSecond> {
     // Agrega más elementos a la lista si es necesario
   ];
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: MenuAppBar(),
-      drawer: MenuDrawer(),
-      body: ListView.builder(
-        itemCount: alimentos.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Card(
-            elevation: 4, // Elevación de la tarjeta
-            margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            child: ListTile(
-              title: Text(
-                '${alimentos[index]['Nombre']}',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Cantidad: ${alimentos[index]['Cantidad']}'),
-                  Text('Fecha de vencimiento: ${alimentos[index]['Fecha_vencimiento']}'),
-                  Text('Categoría: ${alimentos[index]['Categoria']}'),
-                ],
-              ),
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: MenuAppBar(),
+    drawer: MenuDrawer(),
+    body: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(height: 16), 
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              'Lista de alimentos',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
-          );
-        },
-      ),
-    );
-  }
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: alimentos.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Card(
+                elevation: 4,
+                margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                child: ListTile(
+                  title: Text(
+                    '${alimentos[index]['Nombre']}',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Cantidad: ${alimentos[index]['Cantidad']}'),
+                      Text('Fecha de vencimiento: ${DateFormat('dd/MM/yyyy').format(alimentos[index]['Fecha_vencimiento'])}'),
+                      Text('Categoría: ${alimentos[index]['Categoria']}'),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    ),
+  );
+}
 }
