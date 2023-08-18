45ba99bd6afc420abeab80fd7b822d19483974ff
PaulaNino <paulaa_ninob@soy.sea.edu.co>
2023-08-18 07:12:03 -0500

    paula1

--- lib/presentations/springs/screenThird.dart
+++ lib/presentations/springs/screenThird.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ods_alimentos/presentations/springs/screenSecond.dart';
 
+
 import '../windgets/menu_appbar.dart';
 import '../windgets/menu_drawer.dart';
 
@@ -12,7 +13,87 @@ class ScreenThird extends StatefulWidget {
 }
 
 class _ScreenThirdState extends State<ScreenThird> {
-  List<Map<String, dynamic>> alimentos = ScreenSecond.alimentos;
+ List<Map<String, dynamic>> alimentos = [
+    {
+      'Nombre': 'Cerdo',
+      'Cantidad': '10',
+      'Fecha_vencimiento': DateTime(2017, 9, 7, 17, 30),
+      'Categoria': 'Carne',
+    },
+    {
+      'Nombre': 'Pollo',
+      'Cantidad': '12',
+      'Fecha_vencimiento': DateTime(2017, 9, 7, 17, 30),
+      'Categoria': 'Carne',
+    },
+    {
+      'Nombre': 'Lentejas',
+      'Cantidad': '12',
+      'Fecha_vencimiento': DateTime(2017, 9, 7, 17, 30),
+      'Categoria': 'Granos',
+    },
+    {
+        'Nombre': 'Arroz',
+        'Cantidad': '5',
+        'Fecha_vencimiento': DateTime(2023, 10, 15, 14, 0),
+        'Categoria': 'Granos'
+    },
+    {
+        'Nombre': 'Espinaca',
+        'Cantidad': '2',
+        'Fecha_vencimiento': DateTime(2023, 8, 25, 12, 30),
+        'Categoria': 'Vegetales'
+    },
+    {
+        'Nombre': 'Pollo',
+        'Cantidad': '2',
+        'Fecha_vencimiento': DateTime(2023, 8, 20, 18, 15),
+        'Categoria': 'Carnes'
+    },
+    {
+        'Nombre': 'Leche',
+        'Cantidad': '1',
+        'Fecha_vencimiento': DateTime(2023, 9, 2, 10, 0),
+        'Categoria': 'Lacteos'
+    },
+    {
+        'Nombre': 'Manzana',
+        'Cantidad': '8',
+        'Fecha_vencimiento': DateTime(2023, 8, 22, 16, 45),
+        'Categoria': 'Frutas'
+    },
+    {
+        'Nombre': 'Quinua',
+        'Cantidad': '800',
+        'Fecha_vencimiento': DateTime(2023, 9, 10, 13, 15),
+        'Categoria': 'Granos'
+    },
+    {
+        'Nombre': 'Pimiento',
+        'Cantidad': '3',
+        'Fecha_vencimiento': DateTime(2023, 8, 23, 9, 0),
+        'Categoria': 'Vegetales'
+    },
+    {
+        'Nombre': 'Res',
+        'Cantidad': '5',
+        'Fecha_vencimiento': DateTime(2023, 8, 18, 17, 45),
+        'Categoria': 'Carnes'
+    },
+    {
+        'Nombre': 'Yogur',
+        'Cantidad': '24',
+        'Fecha_vencimiento': DateTime(2023, 9, 5, 11, 30),
+        'Categoria': 'Lacteos'
+    },
+    {
+        'Nombre': 'Uvas',
+        'Cantidad': '23',
+        'Fecha_vencimiento': DateTime(2023, 8, 24, 14, 15),
+        'Categoria': 'Frutas'
+    }
+    // Agrega más elementos a la lista si es necesario
+  ];
 
   List<Map<String, dynamic>> obtenerAlimentosProximosAVencer() {
     DateTime today = DateTime.now();
